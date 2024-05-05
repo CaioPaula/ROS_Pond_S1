1c7d2606a979a6f955cc89bdec08cf598f4e491d
CaioPaula <caio.paula@sou.inteli.edu.br>
2024-05-05 18:46:05 -0300

    refactor : corrige código

--- src/entregador_de_jornal/entregador_de_jornal/jornaleiro.py
+++ src/entregador_de_jornal/entregador_de_jornal/jornaleiro.py
@@ -1,32 +1,31 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist 
-from turtlesim.srv import Spawn, Kill
+from turtlesim.srv import Spawn, Kill, SetPen
 import time
-import numpy as np
 
-class Garbelito_Jornaleiro(Node):
+class GarbelitoJornaleiro(Node):
     def __init__(self):
-        super().__init__("garbelito_jornaleiro")
+        super().__init__('garbelito_jornaleiro')
         self.jornaleiro = self.create_publisher(
             msg_type=Twist,
-            topic="/garbelito_bananildo/cmd_vel",  # Assuming the turtle is named "turtle1"
+            topic='/garbelito_bananildo/cmd_vel',  # Assuming the turtle is named "garbelito_bananildo"
             qos_profile=10
         )
 
         # Call the spawn_turtle method to create the turtle
         self.spawn_turtle()
 
-        timer_period = 2
+        timer_period = 0.5
         self.timer = self.create_timer(
-                timer_period_sec=timer_period,
-                callback=self.timer_callback
+                timer_period,
+                self.timer_callback
             )
 
     def timer_callback(self):
         msg = Twist()
-        msg.linear.x = 3.14  # Reduce linear velocity
-        msg.angular.z = 3.14  # Reduce angular velocity
+        msg.linear.x = 10.0  # Reduce linear velocity
+        msg.angular.z = 10.0  # Reduce angular velocity
         self.jornaleiro.publish(msg=msg)
         self.get_logger().info("Olha o jornallllll Garbelitooooooooo !!!!!!!!!")
 
@@ -34,15 +33,15 @@ class Garbelito_Jornaleiro(Node):
         msg = Twist()
         msg.linear.x = distance
         msg.angular.z = angle
-        self.jornaleiro.publish(msg=msg)
+        self.jornaleiro.publish(msg)
         time.sleep(1)  # Adjust sleep time based on the speed of the turtle
 
     def spawn_turtle(self):
-        spawn_client = self.create_client(Spawn, "/spawn")
+        spawn_client = self.create_client(Spawn, '/spawn')
         while not spawn_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().warn('Waiting for /spawn service...')
         request = Spawn.Request()
-        request.name = "garbelito_bananildo"
+        request.name = 'garbelito_bananildo'
         request.x = 5.0
         request.y = 5.0
         request.theta = 0.0
@@ -52,7 +51,8 @@ class Garbelito_Jornaleiro(Node):
         rclpy.spin_until_future_complete(self, future)
 
         if future.result() is not None:
-            self.get_logger().info('Spawned a turtle named "garbelito_banildo"')
+            self.get_logger().info('Spawned a turtle named "garbelito_bananildo"')
+            self.set_pen()
         else:
             self.get_logger().error('Failed to spawn turtle')
 
@@ -64,11 +64,11 @@ class Garbelito_Jornaleiro(Node):
 
     def kill_turtle(self):  
         time.sleep(4)
-        kill_client = self.create_client(Kill, "/kill")
+        kill_client = self.create_client(Kill, '/kill')
         while not kill_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().warn('Waiting for /kill service...')
         kill_request = Kill.Request()
-        kill_request.name = "turtle1"
+        kill_request.name = 'turtle1'
         kill_future = kill_client.call_async(kill_request)
 
         # Block until service call is completed
@@ -79,11 +79,28 @@ class Garbelito_Jornaleiro(Node):
         else:
             self.get_logger().error('Failed to kill turtle')
 
+    def set_pen(self):
+        set_pen_client = self.create_client(SetPen, '/garbelito_bananildo/set_pen')
+        while not set_pen_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().warn('Waiting for /garbelito_bananildo/set_pen service...')
+        set_pen_request = SetPen.Request()
+        set_pen_request.width = 4
+        set_pen_request.r = 255
+        set_pen_request.g = 0
+        set_pen_request.b = 0
+
+        # Call the service to set the pen properties
+        future = set_pen_client.call_async(set_pen_request)
+        rclpy.spin_until_future_complete(self, future)
+        if future.result() is not None:
+            self.get_logger().info('Set pen properties for "garbelito_bananildo"')
+        else:
+            self.get_logger().error('Failed to set pen properties')
 
 def desenha(args=None):
     rclpy.init(args=args)
 
-    jornaleiro = Garbelito_Jornaleiro()
+    jornaleiro = GarbelitoJornaleiro()
 
     rclpy.spin(jornaleiro)
 
@@ -91,5 +108,5 @@ def desenha(args=None):
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     desenha()
